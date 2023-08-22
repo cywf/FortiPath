d69be2b31519a6acaa4bd6ae2aa5e51a70acad5b
cywf <kyle.parisher@owasp.org>
2023-08-22 15:33:40 -0600

    added more functionality to osint web crawler

--- infra/rust/fortipath_osint_crawler/src/main.rs
+++ infra/rust/fortipath_osint_crawler/src/main.rs
@@ -5,6 +5,11 @@
 // to aid in the threat assessment and protective intelligence processes of FortiPath.
 
 use std::collections::HashMap;
+use std::sync::{Arc, Barrier, Mutex};
+use std::time::Duration;
+use tokio::sync::mpsc;
+use tokio_stream::wrappers::ReceiverStream;
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 // TODO: Integrate with popular OSINT tools and libraries for Rust.
 // TODO: Add support for parallel and asynchronous crawling for efficiency.
@@ -50,3 +55,100 @@ fn visualize_data(data: HashMap<String, String>) {
     // TODO: Integrate with a Rust visualization library to create visual representations of the data.
 }
 
+// Advanced architecture for the crawler
+mod advanced_crawler {
+    use std::collections::VecDeque;
+
+    // Define the Spider trait and other relevant structures here
+    pub trait Spider {
+        fn crawl(&self, url: &str) -> Vec<String>; // Returns a list of URLs
+    }
+
+    struct GitHubSpider;
+    impl Spider for GitHubSpider {
+        fn crawl(&self, url: &str) -> Vec<String> {
+            // Logic to crawl GitHub goes here...
+            vec![]
+        }
+    }
+
+    // Define the Downloader trait and other relevant structures here
+    pub trait Downloader {
+        fn download(&self, url: &str) -> String; // Returns the content of the URL
+    }
+
+    struct ReqwestDownloader;
+    impl Downloader for ReqwestDownloader {
+        fn download(&self, url: &str) -> String {
+            // Logic to download using reqwest goes here...
+            String::new()
+        }
+    }
+
+    // Define the logic for respecting robots.txt
+    fn respect_robots_txt(url: &str) -> bool {
+        // Logic to fetch and parse robots.txt and decide if a URL can be crawled
+        true
+    }
+
+    // Define the logic for error handling and retries
+    struct RetryPolicy {
+        max_retries: u8,
+        delay: std::time::Duration,
+    }
+
+    fn retry_download(url: &str, policy: &RetryPolicy, downloader: &impl Downloader) -> Option<String> {
+        let mut attempts = 0;
+        loop {
+            match downloader.download(url) {
+                content if !content.is_empty() => return Some(content),
+                _ if attempts < policy.max_retries => {
+                    std::thread::sleep(policy.delay);
+                    attempts += 1;
+                },
+                _ => return None,
+            }
+        }
+    }
+
+    // Define the logic for swappable store for the queues (e.g., Redis or PostgreSQL)
+    pub trait QueueStore {
+        fn push(&mut self, url: String);
+        fn pop(&mut self) -> Option<String>;
+    }
+
+    struct InMemoryQueue {
+        queue: VecDeque<String>,
+    }
+
+    impl QueueStore for InMemoryQueue {
+        fn push(&mut self, url: String) {
+            self.queue.push_back(url);
+        }
+
+        fn pop(&mut self) -> Option<String> {
+            self.queue.pop_front()
+        }
+    }
+}
+
+// Defense mechanisms against scrapers
+mod defense {
+    // Define the logic for infinite redirects, loops, and slow pages
+    fn trap_for_bots(url: &str) -> bool {
+        // Logic to detect if the URL is a trap and should lead to infinite redirects
+        false
+    }
+
+    // Define the logic for zip bombing
+    fn serve_zip_bomb() -> Vec<u8> {
+        // Logic to serve a zip bomb to the scraper
+        vec![]
+    }
+
+    // Define the logic for serving bad or tainted data
+    fn serve_tainted_data() -> String {
+        // Logic to serve bad or tainted data to the scraper
+        String::from("This is tainted data!")
+    }
+}
